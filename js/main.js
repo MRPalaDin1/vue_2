1004b479e261776c0adf89af63e5b6ead09eac1c
gr421_nepva <mrpaladin228322954@gmail.com>
2024-02-02 15:42:26 +0700

    Second commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 let app = new Vue({
     el: '#app',
-
+    data() {
+        return {
+            columns: [
+                { id: 1, title: 'Столбец1', cards: [] },
+                { id: 2, title: 'Столбец2', cards: [] },
+                { id: 3, title: 'Столбец3', cards: [] },
+            ],
+        };
+    },
 });
